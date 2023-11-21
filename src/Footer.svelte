faed1140adb5204dfb3b7c69bf7389639ef7780c
Matt Wynne <matt@mattwynne.net>
2023-11-21 00:34:19 -0800

    Update footer

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,15 +1,6 @@
 <footer>
-  Made with
-  <span>♥️</span>
-  by
-  <a href="https://koenvangilst.nl">Koen van Gilst</a>
-  <br />
-  Using
-  <a href="https://svelte.dev/">svelte</a>
-  and
-  <a href="https://gun.eco/">gunDB</a>
-  see
-  <a href="https://github.com/vnglst/svelte-gundb-chat">Github</a>
+  See
+  <a href="https://github.com/mattwynne/svelte-automerge-chat">Github</a>
   <br />
   v. {process.env.APP_VERSION} git {process.env.COMMIT_HASH}
 </footer>
