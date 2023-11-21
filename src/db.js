a93cce1fc408a8a81ccd3dcf9824b490f3015e05
Matt Wynne <matt@mattwynne.net>
2023-11-21 00:40:56 -0800

    spaces

--- src/db.js
+++ src/db.js
@@ -1,40 +1,39 @@
 class Topic {
-	constructor(name) {
-		this.name = name
-		this.messages = []
-		this.listeners = []
-	}
+  constructor (name) {
+    this.name = name
+    this.messages = []
+    this.listeners = []
+  }
 
-	set(message) {
-		this.messages.push(message)
-		for(const listener of this.listeners) {
-			listener(message, this.messages.indexOf(message))
-		}
-	}
+  set (message) {
+    this.messages.push(message)
+    for (const listener of this.listeners) {
+      listener(message, this.messages.indexOf(message))
+    }
+  }
 
-	map() {
-		return {
-			on: (listener) => {
-				this.listeners.push(listener)
-			}
-		}
-	}
-	
-	off() {
-		console.log('off')
-	}
+  map () {
+    return {
+      on: listener => {
+        this.listeners.push(listener)
+      }
+    }
+  }
+
+  off () {
+    console.log('off')
+  }
 }
 
 class Db {
-	constructor() {
-		this.topics = {}
-	}
+  constructor () {
+    this.topics = {}
+  }
 
-	get(topic) {
-		if (!this.topics[topic]) this.topics[topic] = new Topic(topic)
-		return this.topics[topic]
-	}
-
+  get (topic) {
+    if (!this.topics[topic]) this.topics[topic] = new Topic(topic)
+    return this.topics[topic]
+  }
 }
 
 export const db = new Db()
